b64dd94d3671d9f90c623467092364623a8eb239
CarVonDark <Tingfei Carlos Feng@DESKTOP-OGU7ECR>
2020-04-19 17:18:03 -0400

    AES encryption done, not tested

--- src/AES.java
+++ src/AES.java
@@ -6,6 +6,7 @@ public class AES extends Converter{
 	private ArrayList<int[][]> inputList;
 	private Sbox sBox;
 	private ArrayList<int[]> keyList;
+	private Polynomial[][] mixMatrix;
 	
 	public AES(String input, String key) {
 		super(input);
@@ -15,15 +16,122 @@ public class AES extends Converter{
 		roundKeysGeneration();
 		this.inputList = new ArrayList<int[][]>();
 		parseInput(input);
-		for(int[][] ints : inputList) {
-			for(int i = 0; i < ints.length;  i++) {
-				for(int j = 0; j < ints[0].length; i++) {
-					System.out.print(ints[i][j]);
-				}
-				System.out.println();
+		this.mixMatrix = new Polynomial[4][4];
+		initializeMixMatrix();
+//		for(int[][] ints : inputList) {
+//			for(int i = 0; i < ints.length;  i++) {
+//				for(int j = 0; j < ints[0].length; i++) {
+//					System.out.print(ints[i][j]);
+//				}
+//				System.out.println();
+//			}
+//		}
+	}
+	
+	public String encrypt() {
+		StringBuilder sb = new StringBuilder();
+		for(int[][] block : inputList) {
+			block = addRoundKey(block, 0);
+			for(int round = 1; round < 11; round++) {
+				block = substituteBytes(block);
+				block = shiftRows(block);
+				block = mixCols(block);
+				block = addRoundKey(block, round);
+			}
+			sb.append(parseInts2String(block));
+		}
+		return sb.toString();
+	}
+	
+	private int[][] addRoundKey(int[][] nums, int round){
+		for(int col = 0; col < nums[0].length; col++) {
+			int[] roundKey = keyList.get(round * 4 + col);
+			for(int row = 0; row < nums.length; row++) {
+				nums[row][col] = nums[row][col] ^ roundKey[row];
 			}
 		}
+		return nums;
 	}
+	
+	private int[][] substituteBytes(int[][] nums) {
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < nums.length; j++) {
+				int row = (nums[i][j] & (15 << 4)) >> 4;
+				int col = nums[i][j] & 15;
+				nums[i][j] = sBox.getSbox(row, col);
+			}
+		}
+		return nums;
+	}
+	
+	private int[][] shiftRows(int[][] nums){
+		for(int i = 0; i < nums[1].length-1; i++) {
+			int temp = nums[1][i];
+			nums[1][i] = nums[1][i+1];
+			nums[1][i+1] = temp;
+		}
+		for(int i = 0; i < nums[2].length-2; i++) {
+			int temp = nums[2][i];
+			nums[2][i] = nums[2][i+2];
+			nums[2][i+2] = temp;
+		}
+		for(int i = 1; i < nums[3].length+1; i++) {
+			int temp = nums[3][i%4];
+			nums[3][i-1] = nums[3][i%4];
+			nums[3][i%4] = temp;
+		}
+		return nums;
+	}
+	
+	private void initializeMixMatrix() {
+		this.mixMatrix[0][0] = new Polynomial(2);
+		this.mixMatrix[0][1] = new Polynomial(3);
+		this.mixMatrix[0][2] = new Polynomial(1);
+		this.mixMatrix[0][3] = new Polynomial(1);
+		
+		this.mixMatrix[1][0] = new Polynomial(1);
+		this.mixMatrix[1][1] = new Polynomial(2);
+		this.mixMatrix[1][2] = new Polynomial(3);
+		this.mixMatrix[1][3] = new Polynomial(1);
+		
+		this.mixMatrix[2][0] = new Polynomial(1);
+		this.mixMatrix[2][1] = new Polynomial(1);
+		this.mixMatrix[2][2] = new Polynomial(2);
+		this.mixMatrix[2][3] = new Polynomial(3);
+		
+		this.mixMatrix[3][0] = new Polynomial(3);
+		this.mixMatrix[3][1] = new Polynomial(1);
+		this.mixMatrix[3][2] = new Polynomial(1);
+		this.mixMatrix[3][3] = new Polynomial(2);
+	}
+	
+	private int[][] mixCols(int[][] nums){
+		for(int col = 0; col < 4; col++) {
+			Polynomial row0 = new Polynomial(nums[0][col]);
+			Polynomial row1 = new Polynomial(nums[1][col]);
+			Polynomial row2 = new Polynomial(nums[2][col]);
+			Polynomial row3 = new Polynomial(nums[3][col]);
+			row0 = Polynomial.add(
+					Polynomial.add(Polynomial.mul(row0, mixMatrix[0][0]), Polynomial.mul(row1, mixMatrix[0][1])),
+					Polynomial.add(Polynomial.mul(row2, mixMatrix[0][2]), Polynomial.mul(row3, mixMatrix[0][3])));
+			row1 = Polynomial.add(
+					Polynomial.add(Polynomial.mul(row0, mixMatrix[1][0]), Polynomial.mul(row1, mixMatrix[1][1])),
+					Polynomial.add(Polynomial.mul(row2, mixMatrix[1][2]), Polynomial.mul(row3, mixMatrix[1][3])));
+			row2 = Polynomial.add(
+					Polynomial.add(Polynomial.mul(row0, mixMatrix[2][0]), Polynomial.mul(row1, mixMatrix[2][1])),
+					Polynomial.add(Polynomial.mul(row2, mixMatrix[2][2]), Polynomial.mul(row3, mixMatrix[2][3])));
+			row3 = Polynomial.add(
+					Polynomial.add(Polynomial.mul(row0, mixMatrix[3][0]), Polynomial.mul(row1, mixMatrix[3][1])),
+					Polynomial.add(Polynomial.mul(row2, mixMatrix[3][2]), Polynomial.mul(row3, mixMatrix[3][3])));
+			nums[0][col] = row0.get();
+			nums[1][col] = row1.get();
+			nums[2][col] = row2.get();
+			nums[3][col] = row3.get();
+		}
+		return nums;
+	}
+	
+	
 	
 	private void roundKeysGeneration() {
 		for(int i = 0; i < key.length - 4; i+=4) {
@@ -112,9 +220,22 @@ public class AES extends Converter{
 		for(int i = 0; i < block.length()-1; i+=2){
 			int firstHalf = parseHexchar2Int(block.charAt(i));
 			int secondHalf = parseHexchar2Int(block.charAt(i+1));
-			re[i/8][(i/2)%4] = (firstHalf << 4) + secondHalf;
+			re[(i/2)%4][i/8] = (firstHalf << 4) + secondHalf;
 		}
 		return re;
+	}
+	
+	private String parseInts2String(int[][] nums) {
+		StringBuilder sb = new StringBuilder();
+		for(int col = 0; col < nums[0].length; col++) {
+			for(int row = 0; row < nums.length; row++) {
+				int firstHalf = (nums[row][col] & (15 << 4)) >> 4;
+				int secondHalf = nums[row][col] & 15;
+				sb.append(Integer.toHexString(firstHalf));
+				sb.append(Integer.toHexString(secondHalf));
+			}
+		}
+		return sb.toString();
 	}
 
 }
