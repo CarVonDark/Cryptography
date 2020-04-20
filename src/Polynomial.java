676bad92bae2d58ae1c7d06c9805cb940d50e7e1
CarVonDark <Tingfei Carlos Feng@DESKTOP-OGU7ECR>
2020-04-20 14:55:16 -0400

    Added citation

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,6 +1,12 @@
+/*
+ * Get idea the mul form github 
+ */
+
 public class Polynomial {
 	
 	private int polynomial;
+	
+	private static final int mod = 283;
 
 	public Polynomial(int a) {
 		this.polynomial = a;
@@ -12,17 +18,15 @@ public class Polynomial {
 	public void set(int a) {
 		this.polynomial = a;
 	}
-	private static final int mod = 283;
+	
 
-	// performs addition modulo GF(2^8)
 	public static Polynomial add(Polynomial p1, Polynomial p2) {
 		int a = p1.get();
 		int b = p2.get();
 		int c = Polynomial.modulus(a ^ b);
 		return new Polynomial(c);
 	}
-
-	// performs multiplication modulo GF(2^8)
+	
 	public static Polynomial mul(Polynomial p1, Polynomial p2) {
 		int product=0;
 
@@ -42,72 +46,6 @@ public class Polynomial {
 		return new Polynomial(product);
 	}
 	
-
-	// returns log2 of a
-	private static int log2(int a) {
-
-		int b = 0;
-		while (a > 0) {
-			a = a >> 1;
-			b = b + 1;
-		}
-
-		return b;
-	}
-
-	// returns quotient when polynomial p1 is divided by p2
-	public static Polynomial quotient(Polynomial p1, Polynomial p2) {
-
-		int a = p1.get();
-		int b = p2.get();
-
-		int q = 0;
-		int t1 = Polynomial.log2(a);
-		int t2 = Polynomial.log2(b);
-
-		while (t1 >= t2) {
-			q = q | (1 << (t1 - t2));
-			a = a ^ (b << (t1 - t2));
-			t1 = Polynomial.log2(a);
-			t2 = Polynomial.log2(b);
-		}
-
-		return new Polynomial(q);
-	}
-
-	// returns inverse of a polynomial in GF(2^8)
-	public static Polynomial inverse(Polynomial p1) {
-
-		Polynomial a1 = new Polynomial(1);
-		Polynomial a2 = new Polynomial(0);
-		Polynomial a3 = new Polynomial(Polynomial.mod);
-
-		Polynomial b1 = new Polynomial(0);
-		Polynomial b2 = new Polynomial(1);
-		Polynomial b3 = new Polynomial(p1.get());
-
-		while (b3.polynomial != 1) {
-
-			Polynomial quo = Polynomial.quotient(a3, b3);
-
-			Polynomial t1 = b1;
-			Polynomial t2 = b2;
-			Polynomial t3 = b3;
-
-			b1 = Polynomial.add(a1, Polynomial.mul(quo, b1));
-			b2 = Polynomial.add(a2, Polynomial.mul(quo, b2));
-
-			b3 = Polynomial.add(a3, Polynomial.mul(quo, b3));
-
-			a1 = t1;
-			a2 = t2;
-			a3 = t3;
-
-		}
-		return b2;
-	}
-
-	// performs modulus operation by GF(2^8)
 	static int modulus(int m) {
 		if (m < 256)
 			return m;
