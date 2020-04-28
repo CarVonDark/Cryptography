778f1cf2b22c3629e9d541480df218e2100574ba
CarVonDark <Tingfei Carlos Feng@DESKTOP-OGU7ECR>
2020-04-28 03:38:36 -0400

    parsing values done

--- src/PohligHellman.java
+++ src/PohligHellman.java
@@ -13,7 +13,7 @@ public class PohligHellman extends Converter{
 	public PohligHellman(String input, String key, String size) {
 		super(input);
 		parseSize(size);
-		parseInput(input);
+		parseInput(parseStringToNumbers(input.toLowerCase()));
 		try {
 			this.key = Integer.parseInt(key);
 		}catch(ArithmeticException e) {
@@ -28,7 +28,7 @@ public class PohligHellman extends Converter{
 
 	private void parseInput(String input) {
 		this.inputList = new ArrayList<BigInteger>();
-		int blockLength = this.blockSize.intValue();
+		int blockLength = this.blockSize.intValue()*2;
 		for(int i = 0; i < input.length()-blockLength; i+=blockLength) {
 			try {
 				BigInteger block = new BigInteger(input.substring(i, i + blockLength));
@@ -50,9 +50,44 @@ public class PohligHellman extends Converter{
 	private void generateKeys() {
 		this.prime = lowerBound.abs();
 		while(true) {
-			
+			if(mr.isPrime(prime))
+				break;
+			else 
+				prime.add(BigInteger.ONE);
 		}
 		
+	}
+	
+	private String parseStringToNumbers(String str) {
+		//Assuming str in lowercase
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < str.length(); i++) {
+			int re = str.charAt(i) - 97;
+			if(re < 10) {
+				sb.append("0" + re);
+			} else {
+				sb.append(re);
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	private String parseNumbersToString(ArrayList<BigInteger> ls) {
+		StringBuilder sb = new StringBuilder();
+		int length = this.blockSize.intValue();
+		for(BigInteger num : ls) {
+			int numValue = num.intValue();
+			StringBuilder sb2 = new StringBuilder();
+			for(int i = 0; i < length; i++) {
+				sb2.append(numValue%100);
+				numValue/=100;
+			}
+			sb.append(sb2.reverse().toString());
+		}
+		
+		return sb.toString();
 	}
 
 }
